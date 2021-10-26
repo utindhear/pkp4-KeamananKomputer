28d3f2298825448136c4fa824014891bc4a6885b
Utin Dhea <92022092+utindhear@users.noreply.github.com>
2021-10-27 00:04:51 +0700

    Add files via upload

---
+++ berhasil_login.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+    if (!isset($_SESSION['username'])) {
+        header("Location: index.php");  
+    }
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Berhasil Login</title>
+</head>
+<body>
+    <div class="container-logout">
+        <form action="" method="POST" class="login-email">
+            <?php echo "<h1>happy to see u, " . $_SESSION['username'] ."!". "</h1>"; ?>
+             
+            <div class="input-group">
+            <a href="logout.php" class="btn">Logout</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
